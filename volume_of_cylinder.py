a631534a1bc4a9bab6d44c526e042bcf7ae0712a
Dahrio-Francois <dahrio.francois@mths.ca>
2022-06-12 00:31:05 +0000

    added volume_of_cylinder.py

---
+++ volume_of_cylinder.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# Created by: Dahrio-Francois
+# Created on: June 2022
+# This code calculates the area of a cylinder
+# using functions
+
+
+def calculate_cylinder_volume(radius, height):
+    # this calcualtes the area
+
+    import math
+
+    # process
+
+    volume_of_cylinder = math.pi * radius**2 * height
+
+    # output
+
+    return volume_of_cylinder
+
+
+def main():
+    # this function collects the radius & height
+
+    try:
+        # input
+
+        radius_from_user = int(input("Enter in the radius of the cylinder (mm): "))
+
+        height_from_user = int(input("\nEnter in the height of the cylinder (mm): "))
+
+        # call function
+        some_var = calculate_cylinder_volume(radius_from_user, height_from_user)
+
+        # process & output
+
+        print(
+            "\nThe volume of a cylinder with those two integers are {0:.2f} mm²".format(
+                some_var
+            )
+        )
+    except Exception:
+        print("\nThose were not valid integers, please input them correctly.")
+
+
+if __name__ == "__main__":
+    main()
